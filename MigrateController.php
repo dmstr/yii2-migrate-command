f8f15e590726accff47d4c90935194f9a0d9c549
Tobias Munk <schmunk@usrbin.de>
2014-10-01 04:30:45 +0200

    fixed database connection display

--- MigrateController.php
+++ MigrateController.php
@@ -144,7 +144,7 @@ class MigrateController extends Controller
 
             $version = Yii::getVersion();
             echo "Yii Migration Tool (based on Yii v{$version})\n\n";
-            echo "Database Connection: " . \Yii::$app->db->dsn . "\n\n";
+            echo "Database Connection: " . $this->db->dsn . "\n\n";
             return true;
         } else {
             return false;
